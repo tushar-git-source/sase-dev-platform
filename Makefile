65b79109e925b5952fe6246c459776a7968ba6a1
Tushar Mukund Kashikar <tuka0821@netcracker.com>
2026-02-25 10:18:23 +0530

    devex

---
+++ Makefile
@@ -0,0 +1,16 @@
+APP_NAME=devex-app
+
+up:
+	docker compose up --build
+
+down:
+	docker compose down
+
+test:
+	docker compose run --rm app pytest
+
+lint:
+	docker compose run --rm app flake8 app.py
+
+clean:
+	docker compose down -v --remove-orphans
---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return jsonify({
+        "service": "devex-sample",
+        "status": "ok"
+        })
+
+@app.get("/products")
+def products():
+    response = requests.get("https://dummyjson.com/products", timeout=10)
+    response.raise_for_status()
+    return jsonify(response.json())
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
